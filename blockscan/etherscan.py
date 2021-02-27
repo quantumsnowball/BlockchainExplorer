a0f0aef9f7c89c66575afd5c44e6edb4bc5b2f22
Quantum Snowball <quantum.snowball@gmail.com>
2021-02-27 20:16:52 +0800

    Successful query from two sources;

--- blockscan/etherscan.py
+++ blockscan/etherscan.py
@@ -1,4 +1,29 @@
 from pathlib import Path
+import requests
+import json
 
 
-ETHERSCAN_API_KEY = (Path(__file__).parent / './api_keys/bscscan').read_text()
+ETHERSCAN_API_KEY = (Path(__file__).parent / './api_keys/etherscan').read_text()
+
+
+def eth_balance(address):
+    url = 'https://api.etherscan.io/api?' \
+          'module=account&' \
+          'action=balance&' \
+          f'address={address}&' \
+          'tag=latest&' \
+          f'apikey={ETHERSCAN_API_KEY}'
+    resp = requests.get(url)
+    obj = json.loads(resp.content)
+    return obj
+
+
+def erc20_token_total_supply(contract_address):
+    url = 'https://api.etherscan.io/api?' \
+          'module=stats&' \
+          'action=tokensupply&' \
+          f'contractaddress={contract_address}&' \
+          f'apikey={ETHERSCAN_API_KEY}'
+    resp = requests.get(url)
+    obj = json.loads(resp.content)
+    return obj
--- blockscan/manager.py
+++ blockscan/manager.py
@@ -3,7 +3,10 @@ import click
 
 @click.command()
 def cli():
-    from .etherscan import ETHERSCAN_API_KEY
+    from .etherscan import eth_balance, erc20_token_total_supply
+    print(eth_balance('0x1311160372Ca71507f69FF8B3Ca2FeF0630dBb26')) # AUTOv2
+    print(erc20_token_total_supply('0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')) # UniswapV2
+    print(erc20_token_total_supply('0x111111111117dc0aa78b770fa6a738034120c302')) # 1INCH Token
     from .bscscan import bep20_token_total_supply
     print(bep20_token_total_supply('0xa184088a740c695e156f91f5cc086a06bb78b827')) # AUTOv2
     print(bep20_token_total_supply('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82')) # PancakeSwap Token
